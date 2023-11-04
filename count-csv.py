b8b6ced495654264669d8b4d052106fd30b92bd6
joiellantero <jllantero@gmail.com>
2023-11-04 11:27:44 +0800

    add argparse

--- count-csv.py
+++ count-csv.py
@@ -1,7 +1,19 @@
 import os
 import csv
+import argparse
 
-path = "./test2"
+parser = argparse.ArgumentParser(description='Quickly count number of entries (rows excluding header) in multiple .csv files')
+
+parser.add_argument(
+    'filepath',
+    metavar='filepath',
+    type=str,
+    help='The folder where all the csv files are located. Enclose the filepath with quotation marks if there are spaces.'
+)
+
+args = parser.parse_args()
+
+path = args.filepath
 files = [file for file in os.listdir(path) if file.endswith(".csv")]
 
 output = dict()
